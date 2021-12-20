64950754301d1dc7c68cb65927a72b65b2484e42
dashixiong <2788347834@qq.com>
2021-12-21 01:30:30 +0800

    添加 登录页面 首页地图切换

--- switch.js
+++ switch.js
@@ -1,11 +1,29 @@
-$('#alert').hide()
-$('#switch-map').click(() => {
-    $('#alert').show()
-    $('#alert').click(() => {
+const My_Switch = {
+    bind: function () {
         $('#alert').hide()
-    })
-})
+        $('#switch-map').click(() => {
+            $('#alert').show()
+            $('#alert').click((e) => {
+                if (e.target === e.currentTarget) {
+                    $('#alert').hide()
+                }
+            })
+        })
+    },
+    render: function ({nodes, callback}) {
+        let zTreeObj;
+        let setting = {
+            callback: {
+                onClick: (event, treeId, treeNode) => {
+                    $('#alert').hide()
+                    callback(event, treeId, treeNode)
+                }
+            }
+        };        // zTree 的参数配置，后面详解
 
-const My_Switch = () => {
+        $(document).ready(function () { zTreeObj = $.fn.zTree.init($("#tree-wrapper"), setting, nodes); });
 
+    },
 }
+My_Switch.bind()
+window.My_Switch = My_Switch
